f7d77190d0151a8a7c58ea9c0ec182cb1b0141cb
Jheam Storch Ross <58143641+srjheam@users.noreply.github.com>
2021-04-17 22:54:16 -0300

    Add SellersController

---
+++ WorkshopASPMVC/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkshopASPMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
